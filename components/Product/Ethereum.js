5fed081107097acc3a1837fbb7a1b3a97067248e
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2022-12-19 22:38:23 -0500

    added 3d ethereum model

--- components/Product/Ethereum.js
+++ components/Product/Ethereum.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import React from "react";
-import ether from "../../public/ether.png";
 import { motion } from "framer-motion";
+import Spline from "@splinetool/react-spline";
 
 export default function Ethereum() {
   return (
-    <div className="bg-black w-[80%] border-[0.5px] text-white h-[63rem] flex items-center justify-center gap-60 product">
-      <div className="">
+    <div className="bg-black w-[80%] border-[0.5px] text-white h-[63rem] flex items-center justify-between gap-60 product">
+      <div className="ml-20">
         <h1 className="text-8xl font-semibold mb-16 border-b-4 w-64 text-ether">
           Ethereum
         </h1>
@@ -19,7 +18,9 @@ export default function Ethereum() {
           <br /> interference.
         </p>
       </div>
-      <Image src={ether} alt="ether" width={350} className="h-[550px]" />
+      <div className="h-[62rem] w-[50rem] ">
+        <Spline scene="https://prod.spline.design/UX5vApJK6vBk771T/scene.splinecode" />
+      </div>
     </div>
   );
 }
